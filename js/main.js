3ea743b31e3bde0d16a81c0668cdd403bb478d4a
Drew Stonebraker <drew.stonebraker@gmail.com>
2016-12-13 11:56:37 +0000

    dynamically fill monthly benefits tooltip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 var today = new Date();
 var currentMonth = today.getMonth();
 
+const MONTHS = {
+  0: 'JAN',
+  1: 'FEB',
+  2: 'MAR',
+  3: 'APR',
+  4: 'MAY',
+  5: 'JUN',
+  6: 'JUL',
+  7: 'AUG',
+  8: 'SEP',
+  9: 'OCT',
+  10: 'NOV',
+  11: 'DEC'
+}
+
 /* ==========================================================================
    USE THESE VARS TO CONFIGURE DASHBOARD FOR INDIVIDUAL CLIENT
    ========================================================================== */
@@ -34,7 +49,7 @@ var clientData = new function() {
   this.lastBackup = new Date('2015-12-14T07:32:00');
   this.monthlyBenefits = '';
 };
-var monthlyBenefits = "<h4>These are your current monthly benefits.</h4><ul><li>Benefit number 1.</li><li>Benefit number 2.</li></ul>";
+var monthlyBenefits = "<h4>Your subscription plan includes:</h4><ul><li>" + clientData.monthlyHours + " hours of work each month</li><li>" + clientData.annualHours + " additional hours of work each year (" + MONTHS[clientData.startingMonth] + " through " + MONTHS[clientData.startingMonth - 1] + ")</li></ul></br>";
 
 //*********************************
 // END OF CONFIGURABLE VARS
@@ -493,26 +508,7 @@ window.onload = function onLoad() {
     //set monthlyBalIteration to appropriate month based on client's starting month
     var monthlyBalIteration = (clientData.startingMonth + i) < 12 ? clientData.startingMonth + i : clientData.startingMonth + i - 12;
 
-    //set month names based on client starting month
-    function getMonthName () {
-      switch (monthlyBalIteration) {
-        case 0: return 'JAN';
-        case 1: return 'FEB';
-        case 2: return 'MAR';
-        case 3: return 'APR';
-        case 4: return 'MAY';
-        case 5: return 'JUN';
-        case 6: return 'JUL';
-        case 7: return 'AUG';
-        case 8: return 'SEP';
-        case 9: return 'OCT';
-        case 10: return 'NOV';
-        case 11: return 'DEC';
-        default: return 'err'
-      }
-    }
-
-    var monthName = getMonthName();
+    var monthName = MONTHS[monthlyBalIteration];
 
     $('#month__name' + i).html(monthName);
 
